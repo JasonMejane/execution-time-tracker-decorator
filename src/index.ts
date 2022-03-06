381ad3fbec2f731a0160460567aa8b3825258d8a
jmejane <51420276+JasonMejane@users.noreply.github.com>
2022-03-06 15:05:44 -0500

    package init + src + test + workflows
    * package init + workflows
    * tsconfig
    * src + test

---
+++ src/index.ts
@@ -0,0 +1,102 @@
+function Sync(params?: ExecutionTimeDecoratorParameters) {
+	return function (target: any, propertyKey: string, descriptor: PropertyDescriptor): PropertyDescriptor {
+		const originalMethod = descriptor.value;
+		const useHrtime = isInNode();
+
+		descriptor.value = function (...args: any[]) {
+			const logParameters: ExecutionTimeLogParameters = {
+				title: getTitle(params?.title, target, propertyKey),
+				start: !useHrtime ? getTime() : 0,
+				startNs: useHrtime ? getTimeNs() : BigInt(0),
+				succeed: true,
+				arguments: params?.shouldLogArguments ? args : undefined,
+				logger: params?.logger,
+			};
+
+			try {
+				const result = originalMethod.apply(this, args);
+				logExecutionTime(logParameters);
+				return result;
+			} catch (err) {
+				logParameters.succeed = false;
+				logExecutionTime(logParameters);
+				throw err;
+			}
+		};
+		return descriptor;
+	};
+}
+
+function Async(params?: ExecutionTimeDecoratorParameters) {
+	return function (target: any, propertyKey: string, descriptor: PropertyDescriptor): PropertyDescriptor {
+		const originalMethod = descriptor.value;
+		const useHrtime = isInNode();
+
+		descriptor.value = async function (...args: any[]) {
+			const logParameters: ExecutionTimeLogParameters = {
+				title: getTitle(params?.title, target, propertyKey),
+				start: !useHrtime ? getTime() : 0,
+				startNs: useHrtime ? getTimeNs() : BigInt(0),
+				succeed: true,
+				arguments: params?.shouldLogArguments ? args : undefined,
+				logger: params?.logger,
+			};
+
+			try {
+				const result = await originalMethod.apply(this, args);
+				logExecutionTime(logParameters);
+				return result;
+			} catch (err) {
+				logParameters.succeed = false;
+				logExecutionTime(logParameters);
+				throw err;
+			}
+		};
+		return descriptor;
+	};
+}
+
+type ExecutionTimeDecoratorParameters = {
+	title?: string;
+	shouldLogArguments?: boolean;
+	logger?: any;
+};
+
+type ExecutionTimeLogParameters = {
+	logger: any;
+	title: string;
+	start: number;
+	startNs: bigint;
+	succeed: boolean;
+	arguments: any[] | undefined;
+};
+
+function getTime(): number {
+	return new Date().valueOf();
+}
+
+function getTimeNs(): bigint {
+	return process.hrtime.bigint();
+}
+
+function getTitle(title: string | undefined, target: any, propertyKey: string): string {
+	return title || `${target instanceof Function ? target.name : target.constructor.name}::${propertyKey}`;
+}
+
+function isInNode(): boolean {
+	return typeof process !== 'undefined' && process.versions != null && process.versions.node != null && typeof process.hrtime !== 'undefined' ;
+}
+
+function logExecutionTime(logParams: ExecutionTimeLogParameters): void {
+	const executionTime = (logParams.start === 0) ? Number(getTimeNs() - logParams.startNs) / 1000 : getTime() - logParams.start;
+
+	(logParams.logger || console).log(`${logParams.title} - ${executionTime}ms - ${logParams.succeed ? 'Success' : 'Failure'}`, {
+		title: logParams.title,
+		executionTime,
+		unit: 'ms',
+		succeed: logParams.succeed,
+		arguments: logParams.arguments,
+	});
+}
+
+export { Async, Sync };
---
+++ test/index.spec.ts
@@ -0,0 +1,251 @@
+import { Async, Sync } from '../src';
+
+export class TestSync {
+	@Sync()
+	testNoParams(limit?: number): void {
+		let a = 0;
+		for (let i = 0; i < (limit ?? 100); i++) {
+			a++;
+		}
+	}
+
+	@Sync({ shouldLogArguments: true })
+	testWithArguments(limit?: number): void {
+		let a = 0;
+		for (let i = 0; i < (limit ?? 100); i++) {
+			a++;
+		}
+	}
+
+	@Sync({ title: 'SyncCustomTitle' })
+	testCustomTitle(limit?: number): void {
+		let a = 0;
+		for (let i = 0; i < (limit ?? 100); i++) {
+			a++;
+		}
+	}
+
+	@Sync({ logger: console })
+	testCustomLogger(limit?: number): void {
+		let a = 0;
+		for (let i = 0; i < (limit ?? 100); i++) {
+			a++;
+		}
+	}
+
+	@Sync()
+	testThrow(limit?: number): void {
+		let a = 0;
+		for (let i = 0; i < (limit ?? 100); i++) {
+			a++;
+		}
+		throw 'Error';
+	}
+}
+
+export class TestAsync {
+	@Async()
+	async testNoParams(limit?: number): Promise<void> {
+		let a = 0;
+		for (let i = 0; i < (limit ?? 2); i++) {
+			a++;
+		}
+	}
+
+	@Async({ shouldLogArguments: true })
+	async testWithArguments(limit?: number): Promise<void> {
+		let a = 0;
+		for (let i = 0; i < (limit ?? 2); i++) {
+			a++;
+		}
+	}
+
+	@Async({ title: 'AsyncCustomTitle' })
+	async testCustomTitle(limit?: number): Promise<void> {
+		let a = 0;
+		for (let i = 0; i < (limit ?? 2); i++) {
+			a++;
+		}
+	}
+
+	@Async({ logger: console })
+	async testCustomLogger(limit?: number): Promise<void> {
+		let a = 0;
+		for (let i = 0; i < (limit ?? 2); i++) {
+			a++;
+		}
+	}
+
+	@Async()
+	async testThrow(limit?: number): Promise<void> {
+		let a = 0;
+		for (let i = 0; i < (limit ?? 2); i++) {
+			a++;
+		}
+		throw 'Error';
+	}
+}
+
+describe('TestSync', () => {
+	let test: TestSync;
+
+	beforeEach(() => {
+		test = new TestSync();
+	});
+
+	it('should run and log default', () => {
+		spyOn(console, 'log').and.callThrough();
+
+		test.testNoParams();
+
+		expect(console.log).toHaveBeenCalledWith('TestSync::testNoParams - 0ms - Success', {
+			title: 'TestSync::testNoParams',
+			executionTime: 0,
+			unit: 'ms',
+			succeed: true,
+			arguments: undefined,
+		});
+	});
+
+	it('should run and log arguments', () => {
+		const arg = 136;
+
+		spyOn(console, 'log').and.callThrough();
+
+		test.testWithArguments(arg);
+
+		expect(console.log).toHaveBeenCalledWith('TestSync::testWithArguments - 0ms - Success', {
+			title: 'TestSync::testWithArguments',
+			executionTime: 0,
+			unit: 'ms',
+			succeed: true,
+			arguments: [arg],
+		});
+	});
+
+	it('should run and log CustomTitle', () => {
+		spyOn(console, 'log').and.callThrough();
+
+		test.testCustomTitle();
+
+		expect(console.log).toHaveBeenCalledWith('SyncCustomTitle - 0ms - Success', {
+			title: 'SyncCustomTitle',
+			executionTime: 0,
+			unit: 'ms',
+			succeed: true,
+			arguments: undefined,
+		});
+	});
+
+	it('should run and log with given logger', () => {
+		spyOn(console, 'log').and.callThrough();
+
+		test.testCustomLogger();
+
+		expect(console.log).toHaveBeenCalledWith('TestSync::testCustomLogger - 0ms - Success', {
+			title: 'TestSync::testCustomLogger',
+			executionTime: 0,
+			unit: 'ms',
+			succeed: true,
+			arguments: undefined,
+		});
+	});
+
+	it('should run, log and throw', () => {
+		spyOn(console, 'log').and.callThrough();
+
+		try {
+			test.testThrow();
+		} catch (err) {}
+
+		expect(console.log).toHaveBeenCalledWith('TestSync::testThrow - 0ms - Failure', {
+			title: 'TestSync::testThrow',
+			executionTime: 0,
+			unit: 'ms',
+			succeed: false,
+			arguments: undefined,
+		});
+	});
+});
+
+describe('TestAsync', () => {
+	let test: TestAsync;
+
+	beforeEach(() => {
+		test = new TestAsync();
+	});
+
+	it('should run and log default', async () => {
+		spyOn(console, 'log').and.callThrough();
+
+		await test.testNoParams();
+
+		expect(console.log).toHaveBeenCalledWith('TestAsync::testNoParams - 0ms - Success', {
+			title: 'TestAsync::testNoParams',
+			executionTime: 0,
+			unit: 'ms',
+			succeed: true,
+			arguments: undefined,
+		});
+	});
+
+	it('should run and log arguments', async () => {
+		const arg = 2;
+
+		spyOn(console, 'log').and.callThrough();
+
+		await test.testWithArguments(arg);
+
+		expect(console.log).toHaveBeenCalledWith('TestAsync::testWithArguments - 0ms - Success', {
+			title: 'TestAsync::testWithArguments',
+			executionTime: 0,
+			unit: 'ms',
+			succeed: true,
+			arguments: [arg],
+		});
+	});
+
+	it('should run and log CustomTitle', async () => {
+		spyOn(console, 'log').and.callThrough();
+
+		await test.testCustomTitle();
+
+		expect(console.log).toHaveBeenCalledWith('AsyncCustomTitle - 0ms - Success', {
+			title: 'AsyncCustomTitle',
+			executionTime: 0,
+			unit: 'ms',
+			succeed: true,
+			arguments: undefined,
+		});
+	});
+
+	it('should run and log with given logger', async () => {
+		spyOn(console, 'log').and.callThrough();
+
+		await test.testCustomLogger();
+
+		expect(console.log).toHaveBeenCalledWith('TestAsync::testCustomLogger - 0ms - Success', {
+			title: 'TestAsync::testCustomLogger',
+			executionTime: 0,
+			unit: 'ms',
+			succeed: true,
+			arguments: undefined,
+		});
+	});
+
+	it('should run, log and throw', async () => {
+		spyOn(console, 'log').and.callThrough();
+
+		try {
+			await test.testThrow();
+		} catch (err) {}
+
+		expect(console.log).toHaveBeenCalledWith('TestAsync::testThrow - 0ms - Failure', {
+			title: 'TestAsync::testThrow',
+			executionTime: 0,
+			unit: 'ms',
+			succeed: false,
+			arguments: undefined,
+		});
+	});
+});
