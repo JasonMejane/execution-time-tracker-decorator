31898540f0c8f154d8d9e4f5d3fae0077aa42781
jmejane <51420276+JasonMejane@users.noreply.github.com>
2022-03-06 21:01:14 -0500

    fix: published code config + workflows

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare function Sync(params?: ExecutionTimeDecoratorParameters): (target: any, propertyKey: string, descriptor: PropertyDescriptor) => PropertyDescriptor;
-declare function Async(params?: ExecutionTimeDecoratorParameters): (target: any, propertyKey: string, descriptor: PropertyDescriptor) => PropertyDescriptor;
-declare type ExecutionTimeDecoratorParameters = {
-    title?: string;
-    shouldLogArguments?: boolean;
-    logger?: any;
-};
-export { Async, Sync };
