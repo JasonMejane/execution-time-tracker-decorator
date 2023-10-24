8f53365b5a50b14e6ba3c007cb0ecea9df3dca27
jmejane <51420276+JasonMejane@users.noreply.github.com>
2023-10-24 10:38:34 -0400

    feat: Update dependencies, add eslint, fix async tests (#29)
    * Fix async test validation
    
    * fix: Update dependencies and fix async tests
    
    * fix: Update dependencies and fix async tests

--- karma.conf.js
+++ karma.conf.js
@@ -2,66 +2,63 @@
 // Generated on Sun Feb 28 2021 09:23:40 GMT-0500 (heure normale de l’Est)
 
 module.exports = function (config) {
-	config.set({
-		// base path that will be used to resolve all patterns (eg. files, exclude)
-		basePath: '',
+    config.set({
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '',
 
-		// frameworks to use
-		// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-		frameworks: ['jasmine', 'karma-typescript'],
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['jasmine', 'karma-typescript'],
 
-		// list of files / patterns to load in the browser
-		files: [
-            { pattern: 'src/**/*.ts' },
-            { pattern: 'test/**/*.spec.ts' }
-		],
+        // list of files / patterns to load in the browser
+        files: [{ pattern: 'src/**/*.ts' }, { pattern: 'test/**/*.spec.ts' }],
 
-		// list of files / patterns to exclude
-		exclude: [],
+        // list of files / patterns to exclude
+        exclude: [],
 
-		plugins: ['karma-jasmine', 'karma-chrome-launcher', 'karma-firefox-launcher', 'karma-coverage', 'karma-typescript'],
+        plugins: ['karma-jasmine', 'karma-chrome-launcher', 'karma-firefox-launcher', 'karma-coverage', 'karma-typescript'],
 
-		// preprocess matching files before serving them to the browser
-		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-		preprocessors: {
-			'src/**/*.ts': ['karma-typescript', 'coverage'],
-			'test/**/*.spec.ts': ['karma-typescript'],
-		},
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+            'src/**/*.ts': ['karma-typescript', 'coverage'],
+            'test/**/*.spec.ts': ['karma-typescript'],
+        },
 
-		// test results reporter to use
-		// possible values: 'dots', 'progress'
-		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
-		reporters: ['progress', 'coverage', 'karma-typescript'],
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['progress', 'coverage', 'karma-typescript'],
 
-		// web server port
-		port: 9876,
+        // web server port
+        port: 9876,
 
-		// enable / disable colors in the output (reporters and logs)
-		colors: true,
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
-		// level of logging
-		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-		logLevel: config.LOG_INFO,
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
-		// enable / disable watching file and executing tests whenever any file changes
-		autoWatch: true,
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
 
-		// start these browsers
-		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-		browsers: ['ChromeHeadless'],
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: ['FirefoxHeadless', 'ChromeHeadless'],
 
-		// Continuous Integration mode
-		// if true, Karma captures browsers, runs the tests and exits
-		singleRun: true,
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: true,
 
-		// Concurrency level
-		// how many browser should be started simultaneous
-		concurrency: Infinity,
+        // Concurrency level
+        // how many browser should be started simultaneous
+        concurrency: Infinity,
 
-		coverageReporter: {
-			includeAllSources: true,
-			dir: 'coverage/',
-			reporters: [{ type: 'html', subdir: 'html' }, { type: 'text-summary' }],
-		},
-	});
+        coverageReporter: {
+            includeAllSources: true,
+            dir: 'coverage/',
+            reporters: [{ type: 'html', subdir: 'html' }, { type: 'text-summary' }],
+        },
+    });
 };
--- src/index.ts
+++ src/index.ts
@@ -1,105 +1,116 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export function ExecTimeSync(params?: ExecutionTimeDecoratorParameters) {
-	return function (target: any, propertyKey: string, descriptor: PropertyDescriptor): any {
-		const originalMethod = descriptor.value;
-		const useHrtime = isInNode();
+    return function (target: unknown, propertyKey: string, descriptor: PropertyDescriptor): void | TypedPropertyDescriptor<() => void> {
+        const originalMethod = descriptor.value;
+        const useHrtime = isInNode();
 
-		descriptor.value = function (...args: any[]) {
-			const logParameters: ExecutionTimeLogParameters = {
-				title: getTitle(params?.title, target, propertyKey),
-				start: !useHrtime ? getTime() : 0,
-				startNs: useHrtime ? getTimeNs() : BigInt(0),
-				succeed: true,
-				arguments: params?.shouldLogArguments ? args : undefined,
-				loggerMethod: params?.loggerMethod,
-			};
+        descriptor.value = function (...args: unknown[]) {
+            const logParameters: ExecutionTimeLogParameters = {
+                title: getTitle(params?.title, target, propertyKey),
+                start: !useHrtime ? getTime() : 0,
+                startNs: useHrtime ? getTimeNs() : BigInt(0),
+                succeed: true,
+                arguments: params?.shouldLogArguments ? args : undefined,
+                loggerMethod: params?.loggerMethod,
+            };
 
-			try {
-				const result = originalMethod.apply(this, args);
-				logExecutionTime(logParameters);
-				return result;
-			} catch (err) {
-				logParameters.succeed = false;
-				logExecutionTime(logParameters);
-				throw err;
-			}
-		};
-		return descriptor;
-	};
+            try {
+                const result = originalMethod.apply(this, args);
+                logExecutionTime(this, logParameters);
+                return result;
+            } catch (err) {
+                logParameters.succeed = false;
+                logExecutionTime(this, logParameters);
+                throw err;
+            }
+        };
+        return descriptor;
+    };
 }
 
 export function ExecTimeAsync(params?: ExecutionTimeDecoratorParameters) {
-	return function (target: any, propertyKey: string, descriptor: PropertyDescriptor): any {
-		const originalMethod = descriptor.value;
-		const useHrtime = isInNode();
+    return function (
+        target: unknown,
+        propertyKey: string,
+        descriptor: PropertyDescriptor,
+    ): void | TypedPropertyDescriptor<() => Promise<void>> {
+        const originalMethod = descriptor.value;
+        const useHrtime = isInNode();
 
-		descriptor.value = async function (...args: any[]) {
-			const logParameters: ExecutionTimeLogParameters = {
-				title: getTitle(params?.title, target, propertyKey),
-				start: !useHrtime ? getTime() : 0,
-				startNs: useHrtime ? getTimeNs() : BigInt(0),
-				succeed: true,
-				arguments: params?.shouldLogArguments ? args : undefined,
-				loggerMethod: params?.loggerMethod,
-			};
+        descriptor.value = async function (...args: unknown[]) {
+            const logParameters: ExecutionTimeLogParameters = {
+                title: getTitle(params?.title, target, propertyKey),
+                start: !useHrtime ? getTime() : 0,
+                startNs: useHrtime ? getTimeNs() : BigInt(0),
+                succeed: true,
+                arguments: params?.shouldLogArguments ? args : undefined,
+                loggerMethod: params?.loggerMethod,
+            };
 
-			try {
-				const result = await originalMethod.apply(this, args);
-				logExecutionTime(logParameters);
-				return result;
-			} catch (err) {
-				logParameters.succeed = false;
-				logExecutionTime(logParameters);
-				throw err;
-			}
-		};
-		return descriptor;
-	};
+            try {
+                const result = await originalMethod.apply(this, args);
+                logExecutionTime(this, logParameters);
+                return result;
+            } catch (err) {
+                logParameters.succeed = false;
+                logExecutionTime(this, logParameters);
+                throw err;
+            }
+        };
+        return descriptor;
+    };
 }
 
 export type ExecutionTimeDecoratorParameters = {
-	title?: string;
-	shouldLogArguments?: boolean;
-	loggerMethod?: any;
+    title?: string;
+    shouldLogArguments?: boolean;
+    loggerMethod?: (msg: string, params: any) => any;
 };
 
 type ExecutionTimeLogParameters = {
-	loggerMethod: any;
-	title: string;
-	start: number;
-	startNs: bigint;
-	succeed: boolean;
-	arguments: any[] | undefined;
+    loggerMethod?: (msg: string, params: any) => any;
+    title: string;
+    start: number;
+    startNs: bigint;
+    succeed: boolean;
+    arguments: any[] | undefined;
 };
 
 function getTime(): number {
-	return new Date().valueOf();
+    return new Date().valueOf();
 }
 
 function getTimeNs(): bigint {
-	return process.hrtime.bigint();
+    return process.hrtime.bigint();
 }
 
 function getTitle(title: string | undefined, target: any, propertyKey: string): string {
-	return title || `${target instanceof Function ? target.name : target.constructor.name}::${propertyKey}`;
+    return title || `${target instanceof Function ? target.name : target.constructor.name}::${propertyKey}`;
 }
 
 function isInNode(): boolean {
-	return typeof process !== 'undefined' && process.versions != null && process.versions.node != null && typeof process.hrtime !== 'undefined' ;
+    return (
+        typeof process !== 'undefined' && process.versions != null && process.versions.node != null && typeof process.hrtime !== 'undefined'
+    );
 }
 
-function logExecutionTime(logParams: ExecutionTimeLogParameters): void {
-	const executionTime = (logParams.start === 0) ? Number(getTimeNs() - logParams.startNs) / 1000000 : getTime() - logParams.start;
-	const logObject = {
-		title: logParams.title,
-		executionTime,
-		unit: 'ms',
-		succeed: logParams.succeed,
-		arguments: logParams.arguments,
-	};
+function logExecutionTime(parentClass: any, logParams: ExecutionTimeLogParameters): void {
+    const executionTime = logParams.start === 0 ? Number(getTimeNs() - logParams.startNs) / 1000000 : getTime() - logParams.start;
+    const logObject = {
+        title: logParams.title,
+        executionTime,
+        unit: 'ms',
+        succeed: logParams.succeed,
+        arguments: logParams.arguments,
+    };
 
-	if (logParams.loggerMethod) {
-		logParams.loggerMethod(`${logParams.title} - ${executionTime}ms - ${logParams.succeed ? 'Success' : 'Failure'}`, logObject);
-	} else {
-		console.log(`${logParams.title} - ${executionTime}ms - ${logParams.succeed ? 'Success' : 'Failure'}`, logObject);
-	}
+    if (logParams.loggerMethod) {
+        logParams.loggerMethod.call(
+            parentClass,
+            `${logParams.title} - ${executionTime}ms - ${logParams.succeed ? 'Success' : 'Failure'}`,
+            logObject,
+        );
+    } else {
+        console.log(`${logParams.title} - ${executionTime}ms - ${logParams.succeed ? 'Success' : 'Failure'}`, logObject);
+    }
 }
